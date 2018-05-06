d1f3626ce7143229c43097ad6954ad9c896a017a
Pavel Vyskocil <vyskocilpavel@muni.cz>
2018-05-06 21:07:59 +0200

    Created exceptions

---
+++ CIA-persistence/src.main/java/cz/fi/muni/CIA/Exceptions/PersonException.java
@@ -0,0 +1,26 @@
+package cz.fi.muni.CIA.Exceptions;
+
+/**
+ * @author Pavel Vyskocil <vyskocilpavel@muni.cz>
+ */
+public class PersonException extends RuntimeException {
+
+	public PersonException() {
+	}
+
+	public PersonException(String message) {
+		super(message);
+	}
+
+	public PersonException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public PersonException(Throwable cause) {
+		super(cause);
+	}
+
+	public PersonException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+}
